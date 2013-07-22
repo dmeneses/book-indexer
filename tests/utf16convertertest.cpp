5a7b0625afe62c51b134b7f945001e8a5a282eb9
Daniela Meneses <daniela11290@gmail.com>
2013-07-22 16:44:30 -0400

    Initial test for utf16

---
+++ tests/utf16convertertest.cpp
@@ -0,0 +1,60 @@
+/*
+ * File:   utf16convertertest.cpp
+ * Author: Daniela
+ *
+ * Created on Jul 22, 2013, 3:48:38 PM
+ */
+
+#include "utf16convertertest.h"
+#include "converter.h"
+#include <iostream>
+
+
+CPPUNIT_TEST_SUITE_REGISTRATION(UTF16ConverterTest);
+
+UTF16ConverterTest::UTF16ConverterTest()
+{
+}
+
+UTF16ConverterTest::~UTF16ConverterTest()
+{
+}
+
+void UTF16ConverterTest::setUp()
+{
+}
+
+void UTF16ConverterTest::tearDown()
+{
+
+}
+
+void UTF16ConverterTest::testUnicodeToUTF16ToGetWithSurrogatedPair()
+{
+    //PRIVATE USE CHARACTER-10FFFD (last Unicode code point)	
+    //UTF16 = DBFF DFFD
+    std::list<long> input;
+    input.push_back(0x10FFFD);
+    std::list<short> output;
+    ConversionResponse result = unicodeToUTF16(input, output);
+    CPPUNIT_ASSERT_EQUAL(ConversionOK, result);
+    CPPUNIT_ASSERT_EQUAL(2, (int) output.size());
+    CPPUNIT_ASSERT_EQUAL((short) 0xDBFF, output.front());
+    output.pop_front();
+    CPPUNIT_ASSERT_EQUAL((short) 0xDFFD, output.front());
+    output.pop_front();
+}
+
+void UTF16ConverterTest::testUnicodeToUTF16ToGetWithoutSurrogatedPair()
+{
+    //CJK UNIFIED IDEOGRAPH-6C34 (water)
+    //UTF16 = 6C34
+    std::list<long> input;
+    input.push_back(0x6C34);
+    std::list<short> output;
+    ConversionResponse result = unicodeToUTF16(input, output);
+    CPPUNIT_ASSERT_EQUAL(ConversionOK, result);
+    CPPUNIT_ASSERT_EQUAL(1, (int) output.size());
+    CPPUNIT_ASSERT_EQUAL((short) 0x6C34, output.front());
+    output.pop_front();
+}
---
+++ tests/utf16convertertest.h
@@ -0,0 +1,34 @@
+/*
+ * File:   utf16convertertest.h
+ * Author: Daniela
+ *
+ * Created on Jul 22, 2013, 3:48:38 PM
+ */
+
+#ifndef UTF16CONVERTERTEST_H
+#define	UTF16CONVERTERTEST_H
+
+#include <cppunit/extensions/HelperMacros.h>
+
+class UTF16ConverterTest : public CPPUNIT_NS::TestFixture
+{
+    CPPUNIT_TEST_SUITE(UTF16ConverterTest);
+
+    CPPUNIT_TEST(testUnicodeToUTF16ToGetWithSurrogatedPair);
+    CPPUNIT_TEST(testUnicodeToUTF16ToGetWithoutSurrogatedPair);
+
+    CPPUNIT_TEST_SUITE_END();
+
+public:
+    UTF16ConverterTest();
+    virtual ~UTF16ConverterTest();
+    void setUp();
+    void tearDown();
+
+private:
+    void testUnicodeToUTF16ToGetWithSurrogatedPair();
+    void testUnicodeToUTF16ToGetWithoutSurrogatedPair();   
+};
+
+#endif	/* UTF16CONVERTERTEST_H */
+
