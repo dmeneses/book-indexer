e35d74423da5c8911d8f58e8dd7a8dbb389b88c6
Daniela Meneses <daniela11290@gmail.com>
2013-07-24 14:26:50 -0400

    Console to perform conversion from utf8 to utf16

--- main.cpp
+++ main.cpp
@@ -12,18 +12,83 @@
 
 
 using namespace std;
+#define CONVERSION "Conversion: "
+#define INDEXER "Indexer: "
+#define MISSING_PARAMENTERS "Missing paramenters. Use -h to check the necessary parameters.\n"
+#define INVALID_OPTION "Invalid option. Use -h to check the necessary parameters and available options.\n"
 
-/*
- * 
- */
+char** arguments;
+int argsCount;
+
+void checkParamenters();
+void convertFile();
+void convertFileBE();
+void convertFileLE();
+
 int main(int argc, char** argv)
 {
-    //    cout << isOn(128, 6);
-    long v = 0x64321;
-    long v1 = v - 0x10000;
-    long vh = v1 >> 10;
-    cout << v1 << endl;
-    cout << vh << endl;
+    if (argc < 2)
+    {
+        cout << MISSING_PARAMENTERS;
+        return 0;
+    }
+
+    arguments = argv;
+    argsCount = argc;
+    checkParamenters();
     return 0;
 }
 
+void checkParamenters()
+{
+    if (strlen(arguments[1]) != 2 || arguments[1][0] != '-')
+    {
+        cout << INVALID_OPTION;
+        return;
+    }
+
+    char option = arguments[1][1];
+
+    switch (option)
+    {
+    case 'c': convertFile();
+        break;
+    default: cout << INVALID_OPTION;
+    }
+}
+
+void convertFile()
+{
+    if (argsCount != 4)
+    {
+        cout << CONVERSION << MISSING_PARAMENTERS;
+        return;
+    }
+
+    if (strlen(arguments[2]) != 2 || arguments[2][0] != '-')
+    {
+        cout << CONVERSION << INVALID_OPTION;
+        return;
+    }
+
+    char option = arguments[2][1];
+
+    switch (option)
+    {
+    case 'l': convertFileBE();
+        break;
+    case 'b': convertFileLE();
+        break;
+    default: cout << CONVERSION << INVALID_OPTION;
+    }
+}
+
+void convertFileBE()
+{
+    cout << "Success";
+}
+
+void convertFileLE()
+{
+    cout << "Success";
+}
